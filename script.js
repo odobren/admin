558cd4c445dfcc895c9a7a740b0e5926bcf1fa33
odobren <167509358+odobren@users.noreply.github.com>
2024-05-07 15:00:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById("workingHoursBtn").addEventListener("click", function() {
+function openSchedule() {
     window.location.href = "https://docs.google.com/spreadsheets/d/1S46rWz7XGKtt21wggySyaFjcHKFvmxsdfjUEFyltZPY/edit#gid=0";
-});
+}
