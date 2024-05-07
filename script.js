7bb70a6126f88a5d5cdce04b37b4c4b44bb8f010
odobren <167509358+odobren@users.noreply.github.com>
2024-05-07 16:35:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function openSchedule() {
-    window.location.href = "https://docs.google.com/spreadsheets/d/1S46rWz7XGKtt21wggySyaFjcHKFvmxsdfjUEFyltZPY/edit#gid=0";
-}
+document.getElementById("openIframeBtn").addEventListener("click", function() {
+    document.getElementById("timeIframe").src = "https://docs.google.com/spreadsheets/d/1S46rWz7XGKtt21wggySyaFjcHKFvmxsdfjUEFyltZPY/edit#gid=0";
+});
